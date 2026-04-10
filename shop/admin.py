9a67210501945e1b40dfe332597dacf71d8d3f48
Og'abek Marimov <kingmarimov@gmail.com>
2026-04-10 11:34:06 +0500

    Add image previews and thumbnail display to ProductAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Category, Product, Company, TelegramUser
 
 @admin.register(Category)
@@ -8,10 +9,10 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category', 'is_featured')
-    list_filter = ('category', 'is_featured')
+    list_display = ('display_thumbnail', 'name', 'price', 'category', 'is_featured', 'ai_status')
+    list_filter = ('category', 'is_featured', 'ai_status')
     search_fields = ('name', 'description')
-    readonly_fields = ('ai_status', 'original_image', 'image_no_bg')
+    readonly_fields = ('display_original', 'display_processed', 'ai_status')
     
     fieldsets = (
         (None, {
@@ -20,18 +21,39 @@ class ProductAdmin(admin.ModelAdmin):
         ('Pricing & Dimensions', {
             'fields': ('price', 'price_per_m2', 'height', 'width')
         }),
-        ('Images', {
-            'fields': ('image', 'original_image', 'image_no_bg')
+        ('Visuals (View Only)', {
+            'fields': ('display_original', 'display_processed'),
+        }),
+        ('Image Management', {
+            'fields': ('image', 'original_image', 'image_no_bg'),
+            'classes': ('collapse',),
         }),
         ('AI Processing', {
             'fields': ('ai_status',),
-            'classes': ('collapse',),
         }),
         ('Discounts', {
             'fields': ('is_on_sale', 'discount_price', 'sale_end_date'),
             'classes': ('collapse',),
         }),
     )
+
+    def display_thumbnail(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="width: 50px; height: auto; border-radius: 4px;" />', obj.image.url)
+        return "-"
+    display_thumbnail.short_description = 'Rasm'
+
+    def display_original(self, obj):
+        if obj.original_image:
+            return format_html('<img src="{}" style="max-width: 300px; height: auto; border: 1px solid #ccc;" />', obj.original_image.url)
+        return "Asl rasm yuklanmagan"
+    display_original.short_description = 'Asl rasm'
+
+    def display_processed(self, obj):
+        if obj.image_no_bg:
+            return format_html('<img src="{}" style="max-width: 300px; height: auto; border: 1px solid #4fb; background: #eee;" />', obj.image_no_bg.url)
+        return "AI rasm hali tayyor emas"
+    display_processed.short_description = 'AI ishlov bergan rasm'
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):

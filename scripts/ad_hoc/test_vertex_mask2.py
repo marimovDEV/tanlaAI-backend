4ce3837c6fe273b2812a2d2de9f88c844ab0af27
marimovDEV <marimovdev@gmail.com>
2026-05-13 16:23:28 +0000

    Remove test/ad_hoc scripts from repo and add to .gitignore

--- scripts/ad_hoc/test_vertex_mask2.py
+++
@@ -1,8 +0,0 @@
-import os, sys, django
-sys.path.append(os.getcwd())
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-django.setup()
-
-from google.genai import types
-
-print(dir(types.EditImageConfig))
